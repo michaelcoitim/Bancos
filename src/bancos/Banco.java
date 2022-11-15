1d908c63a7751947e3b098de1d35cd90d469ad98
michael.coitim <michaelcoitim1@gmail.com>
2022-11-15 18:17:26 -0300

    crinado projeto

--- src/bancos/Banco.java
+++ src/bancos/Banco.java
@@ -1,20 +1,19 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * app banco simples 
  */
 package bancos;
 
 /**
  *
- * @author Jurídico
+ * @author Michael Coitim 
  */
 public class Banco {
 
     /**
-     * @param args the command line arguments
+     *
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
