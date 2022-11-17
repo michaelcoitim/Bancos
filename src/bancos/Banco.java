858d172d8fe89c51c3699fb3562dbacc90cf997d
michael.coitim <michaelcoitim1@gmail.com>
2022-11-16 21:43:31 -0300

    utilizando conta

--- src/bancos/Banco.java
+++ src/bancos/Banco.java
@@ -3,6 +3,8 @@
  */
 package bancos;
 
+import modelos.Conta;
+
 /**
  *
  * @author Michael Coitim 
@@ -10,9 +12,24 @@ package bancos;
 public class Banco {
 
     /**
-     *
+     * criando conta e utilizando seus meteodos
      */
     public static void main(String[] args) {
+        Conta c1 = new Conta("Maria probre");
+        Conta c2 = new Conta("Maria Rica");
+        
+        System.out.println(c1.VerSaldo());
+        
+        c2.Depositar(10000);
+        c1.Depositar(100);
+        c2.Sacar(100);
+        c1.Sacar(200);
+        
+        System.out.println(c1.VerSaldo());
+        System.out.println(c2.VerSaldo());
+
+
+        
         
     }
 
